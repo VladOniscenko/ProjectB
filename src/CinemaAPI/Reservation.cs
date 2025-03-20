7127421b6311ea0880f75dedb32b4e9921339180
DimitriK-04 <dimitrikorenhof@gmail.com>
2025-03-20 13:11:06 +0100

    Added basic Customer class

--- src/CinemaAPI/Reservation.cs
+++ src/CinemaAPI/Reservation.cs
@@ -1,5 +1,10 @@
 public class Reservation{
     public MovieShowing Movie;
     //should multiple seats be able ro be reserved in the same reservation?
-    public Seat seat;
+    public Seat Seat;
+
+    public Reservation(MovieShowing movie, Seat seat){
+        Movie = movie;
+        Seat =  seat;
+    }
 }
