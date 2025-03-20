0d040815634c8c5fd73e2b5fc2ccde65f0b0b745
DimitriK-04 <dimitrikorenhof@gmail.com>
2025-03-20 12:47:58 +0100

    added basic Seat class

---
+++ src/CinemaAPI/Seat.cs
@@ -0,0 +1,15 @@
+public class Seat{
+    //made id and seatnumber different as every auditorium has a seatnumber beginning at 1 (can change if not preffered)
+    public int ID;
+    public int SeatNumber;
+    public bool IsTaken;
+    public string Type;
+
+    public Seat(int id, int seatNumber, bool isTaken, string type){
+        ID = id;
+        SeatNumber = seatNumber;
+        IsTaken = isTaken;
+        Type = type;
+    }
+
+}
