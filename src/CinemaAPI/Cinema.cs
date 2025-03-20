0a32d51eb7594f51314f2ec8c7dfac4a31806e01
DimitriK-04 <dimitrikorenhof@gmail.com>
2025-03-20 12:30:33 +0100

    Added static class Cinema

---
+++ src/CinemaAPI/Cinema.cs
@@ -0,0 +1,12 @@
+using System.Data;
+
+public static class Cinema{
+    static int id;
+    static string CinemaName;
+    static List<Auditorium> Auditoria;
+    const double BasicPrice = 14.0; 
+
+
+
+
+}
