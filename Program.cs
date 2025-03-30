16a2cd4d5446084c7849a3bd6743b66d81032633
Oni <vladoniscenko@outlook.com>
2025-03-30 14:06:38 +0200

    change

--- Program.cs
+++ Program.cs
@@ -1,44 +1,11 @@
-﻿using System;
-using Microsoft.Data.Sqlite;
-using ProjectB.DataAccess;
-using ProjectB.DataModels;
-using ProjectB.Logic;
-
-namespace ProjectB
+﻿namespace ProjectB
 {
     class Program
     {
         static void Main()
         {
-            Console.WriteLine("Hello");
-
-            if (!TestDatabaseConnection())
-            {
-                Console.WriteLine("Database connection failed. Exiting program.");
-                return;
-            }
-            var moviesLogic = new MoviesLogic();
-            Movie createdMovie = moviesLogic.Create();
-
-            Console.WriteLine($"Created Movie: {createdMovie.Title} ({createdMovie.ReleaseDate.Year})");
+            Console.WriteLine("Hello World!");
         }
 
-        static bool TestDatabaseConnection()
-        {
-            try
-            {
-                using (var connection = new SqliteConnection($"Data Source=DataSources/cinema.db"))
-                {
-                    connection.Open();
-                    Console.WriteLine("Database connection successful!");
-                    return true;
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Database connection failed: {ex.Message}");
-                return false;
-            }
-        }
     }
 }
