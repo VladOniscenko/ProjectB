11529984587c85129a8484db3930cda3a3b6740e
Brooklyn <bn.robert@hotmail.com>
2025-04-02 14:38:14 +0200

    Made progress with menu
    Only missing switch/case. Will work on it after pushing this!

---
+++ Presentation/Menu.cs
@@ -0,0 +1,78 @@
+using System;
+using static System.Console;
+
+// Used this video to help me out
+// https://www.youtube.com/watch?v=qAWhGEPMlS8
+
+namespace ProjectB
+{
+    class Menu
+    {
+        private int SelectedIndex = 0;
+        private string[] Options;
+        private string Prompt;
+
+        public Menu(string prompt, string[] options)
+        {
+            Prompt = prompt;
+            Options = options;
+        }
+        
+        public void DisplayOptions()
+        {
+            Console.Clear();
+            WriteLine(Prompt);
+            for (int i = 0; i < Options.Length; i++)
+            {
+                string currentOption = Options[i];
+
+                if (i == SelectedIndex)
+                {
+                    Console.WriteLine($">> {currentOption} <<"); 
+                }
+                else
+                {
+                    Console.WriteLine($"   {currentOption}");
+                }
+            }
+        }
+
+        public int Run()
+        {
+            ConsoleKey keyPressed;
+            do
+            {
+                Console.Clear();
+                DisplayOptions();
+
+                ConsoleKeyInfo keyInfo = ReadKey(true);
+                keyPressed = keyInfo.Key;
+
+                // Code block to update SelectedIndex
+                if (keyPressed == ConsoleKey.UpArrow)
+                {
+                    SelectedIndex = (SelectedIndex - 1 + Options.Length) % Options.Length;
+                }
+                else if (keyPressed == ConsoleKey.DownArrow)
+                {
+                    SelectedIndex = (SelectedIndex + 1) % Options.Length;
+                }
+            }
+            while (keyPressed != ConsoleKey.Enter);
+
+            // Code block for keyPressed cases
+ 
+            return SelectedIndex;
+        }
+
+        
+
+
+        public void Start()
+        {
+            DisplayOptions();
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,33 @@ namespace ProjectB
 {
     class Program
     {
+<<<<<<< Updated upstream
         static void Main()
+=======
+        string prompt = "Welcome customer!";
+        string[] options = { "Register", "Login", "Movies", "About us", "Exit" };
+        Menu menu = new Menu(prompt, options);
+        int SelectedIndex = menu.Run();
+
+        DbFactory.InitializeDatabase();
+        
+        var movieRepo = new MovieRepository();
+        
+        // movieRepo.AddMovie(new Movie 
+        // { 
+        //     Title = "Inception", 
+        //     Description = "A thief who enters the dreams of others to steal secrets.",
+        //     Runtime = 148,
+        //     Actors = "Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page",
+        //     Rating = 8.8,
+        //     Genre = "Sci-Fi",
+        //     AgeRestriction = "PG-13",
+        //     ReleaseDate = new DateTime(2010, 7, 16),
+        //     Country = "USA"
+        // });
+        
+        foreach (Movie movie in movieRepo.GetBestAndNewestMovies())
+>>>>>>> Stashed changes
         {
             Console.WriteLine("Hello World!");
             DbInitializer.Initialize();
