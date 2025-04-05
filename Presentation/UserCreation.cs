ecef5bc3a9c57bc810ae9951f575bba57115d64e
DimitriK-04 <dimitrikorenhof@gmail.com>
2025-04-05 13:57:37 +0200

    Basic question checking

---
+++ Presentation/UserCreation.cs
@@ -0,0 +1,74 @@
+using ProjectB.DataAccess;
+using ProjectB.Models.Users;
+using System.ComponentModel.DataAnnotations;
+
+public static class UserCreation{
+    // needs to check if account was already made and all fields are according to standards
+
+
+    public static void CreateUser(){
+        UserRepository userRepository =  new UserRepository();
+
+
+        User newUser = new User();
+        var email = new EmailAddressAttribute();
+
+        newUser.FirstName = "";
+        while(newUser.FirstName.Length < 3){
+            Console.WriteLine("Please enter your first name");
+            newUser.FirstName = Console.ReadLine();
+        }
+
+        newUser.LastName = "";
+        while(newUser.LastName.Length < 3){
+            Console.WriteLine("Please enter your last name");
+            newUser.LastName = Console.ReadLine();
+        }
+
+        newUser.Email = "";
+        while(!email.IsValid(newUser.Email) && CheckIfUserExists(newUser.Email, userRepository)){
+            Console.WriteLine("Please enter a valid email adress");
+            newUser.Email = Console.ReadLine();
+            if(!CheckIfUserExists(newUser.Email, userRepository)){
+                Console.WriteLine("An account with this email already exists");
+            }
+        }
+
+
+        newUser.Password = "";
+        while(newUser.Password.Length < 4 || newUser.Password.Length > 15 ||
+        !newUser.Password.Any(char.IsUpper) || !newUser.Password.Any(char.IsDigit)){
+            Console.WriteLine("Please enter a valid password (must contain 1 capital letter, 1 number and 1 symbol)");
+            newUser.Password = Console.ReadLine();
+        }
+
+        newUser.IsAdmin = false;
+        // var defaultColor1 = Console.ForegroundColor;
+        // var defaultColor2 = Console.BackgroundColor;
+        // Console.BackgroundColor = defaultColor1;
+        // Console.ForegroundColor = defaultColor2;
+
+
+
+
+
+        Console.WriteLine("huh?");
+
+
+        userRepository.AddUser(newUser);
+    }
+
+public static bool CheckIfUserExists(string email, UserRepository userRepository){
+    foreach(User user in userRepository.GetAllUsers()){
+        if(user.Email == email){
+            return false;
+        }
+    }
+
+
+    return true;
+
+}
+
+}
+
