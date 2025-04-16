e306dc3e0fdb4b2d92540567456cec815620b2c8
Oni <vladoniscenko@outlook.com>
2025-04-16 10:30:58 +0200

    improved readability

--- Presentation/SeatSelection.cs
+++ Presentation/SeatSelection.cs
@@ -7,10 +7,9 @@ public class SeatSelection
 {
     public readonly Showtime? SelectedShowtime;
     public readonly Movie? SelectedMovie;
-    public readonly Auditorium? SelectedAuditorium;
-    public readonly IEnumerable<SeatReservation>? ReservedSeats;
     public readonly IEnumerable<Seat>? Seats;
-    public readonly List<int>? ReservedSeatIds;
+    public Seat? SelectedSeat {get; private set;} = null;
+
 
     public SeatSelection(int showtimeId)
     {
@@ -22,30 +21,21 @@ public class SeatSelection
         }
 
         SelectedMovie = MovieLogic.Find(SelectedShowtime.MovieId);
-        SelectedAuditorium = AuditoriumLogic.Find(SelectedShowtime.AuditoriumId);
-        if (SelectedMovie == null || SelectedAuditorium == null)
+        Seats = SeatLogic.GetSeatsByShowtime(SelectedShowtime.Id);
+        if (SelectedMovie == null || Seats == null || Seats.Count() <= 0)
         {
-            ConsoleMethods.Error("Movie or Auditorium was not found!");
+            ConsoleMethods.Error("Something went wrong! Get in touch with our Customer service!");
             return;
         }
-        
-        Seats = SeatLogic.GetSeatsByAuditorium(SelectedAuditorium.Id);
-        if (Seats == null || Seats.Count() <= 0)
-        {
-            ConsoleMethods.Error("Seats not found!");
-            return;
-        }
-        
-        ReservedSeats = SeatReservationLogic.GetReservedSeatsByShowtimeId(SelectedShowtime.Id);
-        ReservedSeatIds = ReservedSeats.Select(s => s.SeatId).ToList();
+
+        SelectedSeat = Seats.FirstOrDefault(s => s.Active == 1);
     }
-    public void SelectSeats(int showtimeId)
+    public void SelectSeats()
     {
-        Seat? currentSeat = null;
         while (true)
         {
             Console.Clear();
-            PrintSeatSelectionRules();
+            PrintSeatSelectionContent();
 
             int? currentRow = null;
             foreach (var seat in Seats)
@@ -53,50 +43,35 @@ public class SeatSelection
                 if (currentRow != seat.Row)
                 {
                     if (currentRow != null)
+                    {
                         Console.WriteLine();
+                    }
+                    currentRow = seat.Row;
 
                     Console.ForegroundColor = ConsoleColor.Magenta;
                     Console.Write($"{seat.Row:D2} |     ");
                     Console.ResetColor();
-                    currentRow = seat.Row;
                 }
 
-                string type = "DS";
-                if (seat.Type == "love_seat")
+                ConsoleColor? seatColor = seat.Type switch
                 {
-                    type = "LV";
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                }
-                else if (seat.Type == "vip")
+                    "love_seat" => ConsoleColor.Yellow,
+                    "vip" => ConsoleColor.Red,
+                    "normal" => ConsoleColor.Blue,
+                    _ => null
+                };
+
+                if (seatColor != null)
                 {
-                    type = "VP";
-                    Console.ForegroundColor = ConsoleColor.Red;
-                }
-                else if (seat.Type == "normal")
-                {
-                    type = "NO";
-                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.ForegroundColor = seatColor.Value;
                 }
 
-                if (seat.Active == 1)
+                if (SelectedSeat.Id == seat.Id)
                 {
-                    if (currentSeat == null)
-                    {
-                        currentSeat = seat;
-                    }
-
-                    if (currentSeat.Id == seat.Id)
-                    {
-                        Console.BackgroundColor = ConsoleColor.White;
-                    }
+                    Console.BackgroundColor = ConsoleColor.White;
                 }
 
-                string seatContent = " ";
-                if (ReservedSeatIds.Contains(seat.Id))
-                {
-                    seatContent = "X";
-                }
-                
+                string seatContent = seat.Taken == 1 ? "X" : " ";
                 Console.Write(seat.Active == 1 ? $"[{seatContent}]" : "   ");
                 Console.ResetColor();
                 Console.Write(" ");
@@ -104,62 +79,58 @@ public class SeatSelection
             
             Console.WriteLine();
             ConsoleKeyInfo pressedKey = Console.ReadKey();
-
-            if (pressedKey.Key == ConsoleKey.RightArrow || pressedKey.Key == ConsoleKey.D)
-            {
-                var nextSeat = Seats.FirstOrDefault(s => s.Row == currentSeat.Row && s.Number == currentSeat.Number + 1 && s.Active == 1);
-                if (nextSeat != null)
-                {
-                    currentSeat = nextSeat;
-                }
-            }
-            else if (pressedKey.Key == ConsoleKey.LeftArrow || pressedKey.Key == ConsoleKey.A)
-            {
-                var nextSeat = Seats.FirstOrDefault(s => s.Row == currentSeat.Row && s.Number == currentSeat.Number - 1 && s.Active == 1);
-                if (nextSeat != null)
-                {
-                    currentSeat = nextSeat;
-                }
-            }
-            else if (pressedKey.Key == ConsoleKey.DownArrow || pressedKey.Key == ConsoleKey.S)
-            {
-                var nextSeat = Seats.FirstOrDefault(s => s.Row == currentSeat.Row - 1 && s.Number == currentSeat.Number && s.Active == 1);
-                if (nextSeat != null)
-                {
-                    currentSeat = nextSeat;
-                }
-            }
-            else if (pressedKey.Key == ConsoleKey.UpArrow || pressedKey.Key == ConsoleKey.W)
-            {
-                var nextSeat = Seats.FirstOrDefault(s => s.Row == currentSeat.Row + 1 && s.Number == currentSeat.Number && s.Active == 1);
-                if (nextSeat != null)
-                {
-                    currentSeat = nextSeat;
-                }
-            }
+            SelectedSeat = Move(pressedKey);
         }
     }
     
-    static void PrintSeatSelectionRules()
+    private void PrintSeatSelectionContent()
     {
+        Console.WriteLine("Selected movie:");
+        Console.WriteLine(SelectedMovie);
+        Console.WriteLine();
+
         Console.WriteLine("Rules:");
 
         Console.ForegroundColor = ConsoleColor.Red;
-        Console.Write("[X]");
-        Console.ResetColor();
-        Console.WriteLine(" - Reserved (already taken)");
+        Console.WriteLine("[X] - Reserved (already taken)");
 
         Console.ForegroundColor = ConsoleColor.Green;
-        Console.Write("[ ]");
-        Console.ResetColor();
-        Console.WriteLine(" - Available (free)");
+        Console.WriteLine("[ ] - Available (free)");
 
         Console.ForegroundColor = ConsoleColor.Yellow;
-        Console.Write("[S]");
-        Console.ResetColor();
-        Console.WriteLine(" - Selected (your choice)");
+        Console.WriteLine("[S] - Selected (your choice)");
 
         Console.WriteLine();
+        Console.ResetColor();
+    }
+
+    private Seat Move(ConsoleKeyInfo pressedKey)
+    {
+        // decide direction
+        int row = pressedKey.Key switch
+        {
+            (ConsoleKey.UpArrow or ConsoleKey.W) => 1,
+            (ConsoleKey.DownArrow or ConsoleKey.S) => -1,
+            _ => 0
+        };
+        
+        int number = pressedKey.Key switch
+        {
+            (ConsoleKey.D or ConsoleKey.RightArrow) => 1,
+            (ConsoleKey.LeftArrow or ConsoleKey.A) => -1,
+            _ => 0
+        };
+        
+        // search for matching seat
+        var nextSeat = Seats.FirstOrDefault(
+            s => 
+                s.Row == SelectedSeat.Row + row && 
+                s.Number == SelectedSeat.Number + number && 
+                s.Active == 1
+        );
+        
+        // if seat not found return current seat. otherwise return found seat
+        return nextSeat == null ? SelectedSeat : nextSeat;
     }
 
 }
