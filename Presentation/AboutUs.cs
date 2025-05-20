e21bd8fef6e21e9ff9e52d435e669ecd0170a224
DimitriK-04 <dimitrikorenhof@gmail.com>
2025-05-20 22:40:54 +0200

    removed unneeded comments

--- Presentation/AboutUs.cs
+++ Presentation/AboutUs.cs
@@ -28,20 +28,3 @@ public class AboutUs
         Console.ReadKey();
     }
 }
-
-// the following information we know
-// info owner:
-// Jake Darcy is an entrepreneur that is planning to set up a typical Dutch cinema chain 
-// where going to the movies becomes a movie experience and where lounging is the key word. 
-// In the Netherlands there is quite a lack of this. His plan is to start setting up a cinema 
-// in the city of Rotterdam. The cinema will consist of a bar, a lounge area (where visitors can wait) 
-// and three auditoriums with seats of 150 -, 300 - and 500 respectively. If the formula proves successful, 
-// there are plans to expand to other major cities in the Netherlands, such as Amsterdam, Utrecht and The Hague.
-
-// Since Jake is a film lover as well, you know what you are talking about. Your vision is to provide the cinema with the latest technologies, such as Auro 3D certified cinema sound system, IMAX 3D digital certified visual projectors and super comfortable (VIP) seats with plenty of legroom and space in between. Jake realises that this is not enough to attract consumers; after all, competition in this market segment is fierce. The Cinema therefore needs to excel from the competition in certain respects.
-
-// info location:
-// The cinema will be located in the heart of Rotterdam, namely: "Wijnhaven 107, 3011 WN in Rotterdam". User-friendliness will be at the top of the list of priorities. This is reflected in everything that has to do with the cinema. Jake also devised a unique way to classify the theatres and determine the price per seat. Every cinema lover understands that not every seat in a cinema offers the same cinematic experience. Think, for example, of sitting in the front row in the corner, in the back, or sitting in the middle of the auditorium. All three locations offer a different cinematic experience and therefore have a different price tag. The idea of the rooms is shown schematically below.
-
-// info type of seats:
-// three kinds: standard, vip, premium
